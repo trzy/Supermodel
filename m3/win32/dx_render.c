e546c45be5f610c7965700c85fc67c6df45a9216
vlinde <>
2003-07-20 22:01:12 +0000

    Added vrom texturing and changed texturing to use two pages.

--- m3/win32/dx_render.c
+++ m3/win32/dx_render.c
@@ -46,7 +46,7 @@ static LPDIRECT3DVERTEXBUFFER9	vertex_buffer;
 static POLYGON_LIST polygon_list[32768];
 
 static LPDIRECT3DTEXTURE9	texture[4096];
-static short texture_table[64*64];
+static short texture_table[2][64*32];
 
 static D3DFORMAT supported_formats[] = { D3DFMT_A1R5G5B5,
 										 D3DFMT_A8R8G8B8,
@@ -339,7 +339,7 @@ static void decode_texture8(UINT8 *ptr, UINT8 *src, int width, int height)
 				for( x=0; x<8; x++) {
 					int x2 = ((x / 2) * 4) + (x & 0x1);
 					int y2 = ((y / 2) * 16) + ((y & 0x1) * 2);
-					UINT8 pix = src[index + x2 + y2 ^1];
+					UINT8 pix = src[index + x2 + y2 ^ 3];
 
 					ptr[((j+y) * width) + i + x] = pix;
 				}
@@ -353,7 +353,7 @@ void osd_renderer_upload_texture(UINT8 *src)
 {
 	UINT32 header[2];
 	int texture_num, i, j;
-	int width, height, xpos, ypos, type, depth;
+	int width, height, xpos, ypos, type, depth, page;
 	D3DLOCKED_RECT	locked_rect;
 	HRESULT hr;
 	UINT8 *ptr;
@@ -373,15 +373,17 @@ void osd_renderer_upload_texture(UINT8 *src)
 	}
 
 	// Get the texture information from the header
+	
 	header[0] = *(UINT32*)(&src[0]);
 	header[1] = *(UINT32*)(&src[4]);
-	
+
 	width	= 32 << ((header[1] >> 14) & 0x7);
 	height	= 32 << ((header[1] >> 17) & 0x7);
 	xpos	= (header[1] & 0x3F) * 32;
 	ypos	= ((header[1] >> 7) & 0x1F) * 32;
 	type	= (header[1] >> 24) & 0xFF;
 	depth	= (header[1] & 0x800000) ? 1 : 0;
+	page	= (header[1] & 0x100000) ? 1 : 0;
 
 	if(type != 1)		// TODO: Handle mipmaps
 		return;
@@ -411,7 +413,119 @@ void osd_renderer_upload_texture(UINT8 *src)
 	// Update texture table
 	for( j = ypos/32; j < (ypos+height)/32; j++) {
 		for( i = xpos/32; i < (xpos+width)/32; i++) {
-			texture_table[(j*64) + i] = texture_num;
+			texture_table[page][(j*64) + i] = texture_num;
+			// TODO: Remove overwritten textures
+		}
+	}
+}
+
+static void decode_vrom_texture16(UINT16* ptr, UINT16* src, int width, int height)
+{
+	int i, j, x, y;
+	int index = 0;
+
+	for( j=0; j<height; j+=8 ) {
+		for( i=0; i<width; i+=8 ) {
+
+			// Decode tile
+			for( y=0; y<8; y++) {
+				for( x=0; x<8; x++) {
+					int x2 = ((x / 2) * 4) + (x & 0x1);
+					int y2 = ((y / 2) * 16) + ((y & 0x1) * 2);
+					UINT16 pix = BSWAP16(src[index + x2 + y2]);
+
+					ptr[((j+y) * width) + i + x] = pix;
+				}
+			}
+			index += 64;
+		}
+	}
+}
+
+static void decode_vrom_texture8(UINT8 *ptr, UINT8 *src, int width, int height)
+{
+	int i, j, x, y;
+	int index = 0;
+
+	for( j=0; j<height; j+=8 ) {
+		for( i=0; i<width; i+=8 ) {
+
+			// Decode tile
+			for( y=0; y<8; y++) {
+				for( x=0; x<8; x++) {
+					int x2 = ((x / 2) * 4) + (x & 0x1);
+					int y2 = ((y / 2) * 16) + ((y & 0x1) * 2);
+					UINT8 pix = src[index + x2 + y2];
+
+					ptr[((j+y) * width) + i + x] = pix;
+				}
+			}
+			index += 64;
+		}
+	}
+}
+
+void osd_renderer_upload_vrom_texture(UINT32 header, UINT32 length, UINT8 *src)
+{
+	int texture_num, i, j;
+	int width, height, xpos, ypos, type, depth, page;
+	D3DLOCKED_RECT	locked_rect;
+	HRESULT hr;
+	UINT8 *ptr;
+	D3DFORMAT	format = 0;
+
+	// Find a free D3D texture
+	texture_num = -1;
+	i = 0;
+	while( i < 4096 && texture_num == -1) {
+		if(texture[i] == NULL) {
+			texture_num = i;
+		}
+		i++;
+	}
+	if(texture_num < 0) {
+		osd_error("DirectX Error: No free textures available !\n");
+	}
+
+	// Get the texture information from the header
+
+	width	= 32 << ((header >> 14) & 0x7);
+	height	= 32 << ((header >> 17) & 0x7);
+	xpos	= (header & 0x3F) * 32;
+	ypos	= ((header >> 7) & 0x1F) * 32;
+	type	= (header >> 24) & 0xFF;
+	depth	= (header & 0x800000) ? 1 : 0;
+	page	= (header & 0x100000) ? 1 : 0;
+
+	if(type != 1)		// TODO: Handle mipmaps
+		return;
+
+	format = (depth) ? D3DFMT_A1R5G5B5 : D3DFMT_L8;
+
+	// Create D3D texture
+	hr = D3DXCreateTexture( device, width, height, 1, 0, format, D3DPOOL_MANAGED,
+							&texture[texture_num] );
+	if( FAILED(hr) )
+		osd_error("D3DXCreateTexture failed.\n");
+	
+	hr = IDirect3DTexture9_LockRect( texture[texture_num], 0, &locked_rect, NULL, 0 );
+	if( FAILED(hr) )
+		osd_error("IDirect3DTexture9_LockRect failed.\n");
+
+	ptr = (UINT8*)locked_rect.pBits;
+
+	// Decode the texture
+	if(depth) {
+		decode_vrom_texture16( (UINT16*)&ptr[0], (UINT16*)&src[0], width, height );
+	} else {
+		decode_vrom_texture8( ptr, &src[0], width, height );
+	}
+	IDirect3DTexture9_UnlockRect( texture[texture_num], 0 );
+
+	// Update texture table
+	for( j = ypos/32; j < (ypos+height)/32; j++) {
+		for( i = xpos/32; i < (xpos+width)/32; i++) {
+			texture_table[page][(j*64) + i] = texture_num;
 			// TODO: Remove overwritten textures
 		}
 	}
@@ -435,7 +549,7 @@ static void render_model(UINT32 *src)
 {
 	VERTEX vertex[4];
 	VERTEX prev_vertex[4];
-	int end, index, polygon_index, vb_index, prev_texture, i;
+	int end, index, polygon_index, vb_index, prev_texture, i, page;
 	VERTEX  *vb;
 	HRESULT hr;
 
@@ -534,7 +648,8 @@ static void render_model(UINT32 *src)
 		// Get the texture number from texture table
 		texture_x = ((header[4] & 0x1F) << 1) | ((header[5] >> 7) & 0x1);
 		texture_y = (header[5] & 0x1F);
-		tex_num = texture_table[texture_y * 64 + texture_x];
+		page = (header[4] & 0x40) ? 1 : 0;
+		tex_num = texture_table[page][texture_y * 64 + texture_x];
 		if((header[6] & 0x4000000) == 0)
 			tex_num = -1;
 
@@ -586,7 +701,7 @@ static void render_model_vrom(UINT32 *src)
 {
 	VERTEX vertex[4];
 	VERTEX prev_vertex[4];
-	int end, index, polygon_index, vb_index, prev_texture, i;
+	int end, index, polygon_index, vb_index, prev_texture, i, page;
 	VERTEX  *vb;
 	HRESULT hr;
 
@@ -685,7 +800,8 @@ static void render_model_vrom(UINT32 *src)
 		// Get the texture number from texture table
 		texture_x = ((header[4] & 0x1F) << 1) | ((header[5] >> 7) & 0x1);
 		texture_y = (header[5] & 0x1F);
-		tex_num = texture_table[texture_y * 64 + texture_x];
+		page = (header[4] & 0x40) ? 1 : 0;
+		tex_num = texture_table[page][texture_y * 64 + texture_x];
 		if((header[6] & 0x4000000) == 0)
 			tex_num = -1;
 
