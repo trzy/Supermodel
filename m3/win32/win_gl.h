5eeba7672597441cc7c42a86f8cc87cdcf012e66
trzy <>
2003-12-23 00:54:56 +0000

    New rendering system.

---
+++ m3/win32/win_gl.h
@@ -0,0 +1,13 @@
+/*
+ * win32/win_gl.h
+ *
+ * Windows OpenGL header.
+ */
+
+#ifndef INCLUDED_WIN32_WIN_GL_H
+#define INCLUDED_WIN32_WIN_GL_H
+
+extern void win_gl_init(UINT, UINT);
+extern void win_gl_shutdown(void);
+
+#endif  // INCLUDED_WIN32_WIN_GL_H
