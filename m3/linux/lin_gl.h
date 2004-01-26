ccb55ecfa745a96c1a5baf28ccebb753f813e2be
trzy <>
2004-01-26 03:37:24 +0000

    Linux update by RB (forgot to add this with the other files.)

---
+++ m3/linux/lin_gl.h
@@ -0,0 +1,13 @@
+/*
+ * linux/lin_gl.h
+ *
+ * Linux OpenGL header.
+ */
+
+#ifndef INCLUDED_LINUX_LIN_GL_H
+#define INCLUDED_LINUX_LIN_GL_H
+
+extern void lin_gl_init(UINT, UINT);
+extern void lin_gl_shutdown(void);
+
+#endif  // INCLUDED_LINUX_LIN_GL_H
