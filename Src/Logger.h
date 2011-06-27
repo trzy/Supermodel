2465354ef680a20f90531f24c2c581879e9e2226
Nik Henson <nik-m3@diablo-designs.co.uk>
2011-06-27 23:21:43 +0000

    New CLogger class that handles logging from Supermodel.  Added so that log messages can be routed through debugger if required, rather than printed to console.

---
+++ Src/Logger.h
@@ -0,0 +1,45 @@
+#ifndef INCLUDED_LOGGER_H
+#define INCLUDED_LOGGER_H
+
+#include <stdarg.h>
+
+/*
+ * CLogger
+ * 
+ * Abstract class that receives log messages from Supermodel.
+ */
+class CLogger
+{
+public:
+	void DebugLog(const char *fmt, ...)
+	{
+		va_list vl;
+		va_start(vl, fmt);
+		DebugLog(fmt, vl);
+		va_end(vl);
+	}
+
+	void InfoLog(const char *fmt, ...)
+	{
+		va_list vl;
+		va_start(vl, fmt);
+		InfoLog(fmt, vl);
+		va_end(vl);
+	}
+
+	void ErrorLog(const char *fmt, ...)
+	{
+		va_list vl;
+		va_start(vl, fmt);
+		ErrorLog(fmt, vl);
+		va_end(vl);
+	}
+
+	virtual void DebugLog(const char *fmt, va_list vl) = 0;
+
+	virtual void InfoLog(const char *fmt, va_list vl) = 0;
+	
+	virtual void ErrorLog(const char *fmt, va_list vl) = 0;
+};
+
+#endif	// INCLUDED_LOGGER_H
