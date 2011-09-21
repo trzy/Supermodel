55e6a843bb419cfb25eaf3915f6db8cc9338e2b7
Nik Henson <nik-m3@diablo-designs.co.uk>
2011-09-21 22:53:41 +0000

    Added copyright headers to all debugger files and replaced C standard library headers with c++ versions

--- Src/Debugger/CPU/68KDebug.cpp
+++ Src/Debugger/CPU/68KDebug.cpp
@@ -1,9 +1,34 @@
+/**
+ ** Supermodel
+ ** A Sega Model 3 Arcade Emulator.
+ ** Copyright 2011 Bart Trzynadlowski, Nik Henson
+ **
+ ** This file is part of Supermodel.
+ **
+ ** Supermodel is free software: you can redistribute it and/or modify it under
+ ** the terms of the GNU General Public License as published by the Free 
+ ** Software Foundation, either version 3 of the License, or (at your option)
+ ** any later version.
+ **
+ ** Supermodel is distributed in the hope that it will be useful, but WITHOUT
+ ** ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ ** FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ ** more details.
+ **
+ ** You should have received a copy of the GNU General Public License along
+ ** with Supermodel.  If not, see <http://www.gnu.org/licenses/>.
+ **/
+
+/*
+ * 68KDebug.cpp
+ */
+
 #ifdef SUPERMODEL_DEBUGGER
 
 #include "68KDebug.h"
 
-#include <string.h>
-#include <ctype.h>
+#include <cctype>
+#include <string>
 
 #define M68KSPECIAL_SP 0
 #define M68KSPECIAL_SR 1
--- Src/Debugger/IO.h
+++ Src/Debugger/IO.h
@@ -1,6 +1,31 @@
+/**
+ ** Supermodel
+ ** A Sega Model 3 Arcade Emulator.
+ ** Copyright 2011 Bart Trzynadlowski, Nik Henson
+ **
+ ** This file is part of Supermodel.
+ **
+ ** Supermodel is free software: you can redistribute it and/or modify it under
+ ** the terms of the GNU General Public License as published by the Free 
+ ** Software Foundation, either version 3 of the License, or (at your option)
+ ** any later version.
+ **
+ ** Supermodel is distributed in the hope that it will be useful, but WITHOUT
+ ** ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ ** FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ ** more details.
+ **
+ ** You should have received a copy of the GNU General Public License along
+ ** with Supermodel.  If not, see <http://www.gnu.org/licenses/>.
+ **/
+
+/*
+ * IO.h
+ */
+
 #ifdef SUPERMODEL_DEBUGGER
-#ifndef INCLUDED_IOPORT_H
-#define INCLUDED_IOPORT_H
+#ifndef INCLUDED_IO_H
+#define INCLUDED_IO_H
 
 #include "Types.h"
 
@@ -136,5 +161,5 @@ namespace Debugger
 	}
 }
 
-#endif	// INCLUDED_IOPORT_H
+#endif	// INCLUDED_IO_H
 #endif  // SUPERMODEL_DEBUGGER
