dba953d1a07f872263684a0bc74fb0ecc123038b
Nik Henson <nik-m3@diablo-designs.co.uk>
2011-09-21 22:31:27 +0000

    Added copyright header to file

--- Src/Inputs/InputTypes.cpp
+++ Src/Inputs/InputTypes.cpp
@@ -1,3 +1,30 @@
+/**
+ ** Supermodel
+ ** A Sega Model 3 Arcade Emulator.
+ ** Copyright 2011 Bart Trzynadlowski, Nik Henson
+ **
+ ** This file is part of Supermodel.
+ **
+ ** Supermodel is free software: you can redistribute it and/or modify it under
+ ** the terms of the GNU General Public License as published by the Free 
+ ** Software Foundation, either version 3 of the License, or (at your option)
+ ** any later version.
+ **
+ ** Supermodel is distributed in the hope that it will be useful, but WITHOUT
+ ** ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ ** FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ ** more details.
+ **
+ ** You should have received a copy of the GNU General Public License along
+ ** with Supermodel.  If not, see <http://www.gnu.org/licenses/>.
+ **/
+
+/*
+ * InputTypes.cpp
+ *
+ * Different input subclasses, grouped together here as all are very simple classes.
+ */
+
 #include "Supermodel.h"
 
 // All the input subclasses have been grouped together here as they are very simple classes
