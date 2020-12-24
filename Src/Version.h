53185c3effbc38733fb29669631b6d428564c38a
SpinDizzy <eescargot1@gmail.com>
2020-12-24 10:20:26 +0000

    Introducing a special release build target with SVN version stamping.

--- Src/Version.h
+++ Src/Version.h
@@ -6,7 +6,7 @@
  ** This file is part of Supermodel.
  **
  ** Supermodel is free software: you can redistribute it and/or modify it under
- ** the terms of the GNU General Public License as published by the Free 
+ ** the terms of the GNU General Public License as published by the Free
  ** Software Foundation, either version 3 of the License, or (at your option)
  ** any later version.
  **
@@ -18,16 +18,18 @@
  ** You should have received a copy of the GNU General Public License along
  ** with Supermodel.  If not, see <http://www.gnu.org/licenses/>.
  **/
- 
+
 /*
  * Version.h
- * 
+ *
  * Supermodel version string.
  */
- 
+
 #ifndef INCLUDED_VERSION_H
 #define INCLUDED_VERSION_H
 
+#ifndef SUPERMODEL_VERSION
 #define SUPERMODEL_VERSION	"0.3a-WIP"
+#endif
 
 #endif	// INCLUDED_VERSION_H
