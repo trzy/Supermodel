df7787f0401cf6b0d12b636caccaf8f4d72c9337
Fernando Casas Schössow <casasfernando@outlook.com>
2022-11-14 20:06:09 +0100

    Keep Supermodel files (config, nvram, saves, etc.) in a predictable path when running on Linux

---
+++ Src/OSD/Windows/FileSystemPath.cpp
@@ -0,0 +1,37 @@
+/**
+ ** Supermodel
+ ** A Sega Model 3 Arcade Emulator.
+ ** Copyright 2003-2022 The Supermodel Team
+ **
+ ** This file is part of Supermodel.
+ **
+ ** Supermodel is free software: you can redistribute it and/or modify it under
+ ** the terms of the GNU General Public License as published by the Free
+ ** Software Foundation, either version 3 of the License, or (at your option)
+ ** any later version.
+ **
+ ** Supermodel is distributed in the hope that it will be useful, but WITHOUT
+ ** ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ ** FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ ** more details.
+ **
+ ** You should have received a copy of the GNU General Public License along
+ ** with Supermodel.  If not, see <http://www.gnu.org/licenses/>.
+ **/
+
+#include "FileSystemPath.h"
+#include <string>
+
+namespace FileSystemPath
+{
+    // Generates a path to be used by Supermodel files
+    std::string GetPath(std::string pathType)
+    {
+        if (pathType == "Config") return "Config/";
+        if (pathType == "Screenshots") return "";
+        if (pathType == "Saves") return "Saves/";
+        if (pathType == "NVRAM") return "NVRAM/";
+        if (pathType == "Log") return "";
+        if (pathType == "Analysis") return "Analysis/";
+    }
+}
